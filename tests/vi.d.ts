d6c8c37ae070be7927e1796f4e52f9a046c70673
jonathassardinha <jonathas.sardinha@gmail.com>
2022-06-10 12:11:04 -0300

    chore: added PR workflow
    chore: fixed vi.d.ts

--- tests/vi.d.ts
+++ tests/vi.d.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
 
 declare global {
   namespace Vi {
-    type Assertion<T> = TestingLibraryMatchers<void, T>;
-    type AsymmetricMatchersContaining<T> = TestingLibraryMatchers<void, T>;
+    interface Assertion<T> extends TestingLibraryMatchers<void, T> {}
+    interface AsymmetricMatchersContaining<T>
+      extends TestingLibraryMatchers<void, T> {}
   }
 }
