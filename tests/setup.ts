1667ae732cfc186689b3cf34e9426c02bdd9adac
Jonathas Sardinha <jonathas.sardinha@gmail.com>
2023-06-12 22:50:26 -0300

    update some packages and move to pnpm

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,22 @@
-import extensions from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+/* eslint-disable import/no-extraneous-dependencies */
+import { TextEncoder, TextDecoder } from 'util';
+import 'vitest-dom/extend-expect';
 
-expect.extend(extensions);
+// jsdom doest not have these objects defined in global, so we need to polyfill it
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+
+// This is needed for the react-slick package
+const mockMatchMedia = () => ({
+  matches: false,
+  addListener() {},
+  removeListener() {},
+});
+
+global.matchMedia = global.matchMedia || mockMatchMedia;
+
+const addActType = (obj: object) =>
+  obj as typeof obj & { IS_REACT_ACT_ENVIRONMENT: boolean };
+
+addActType(global).IS_REACT_ACT_ENVIRONMENT = true;
+addActType(globalThis).IS_REACT_ACT_ENVIRONMENT = true;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
 import { defineConfig } from 'vitest/config';
+import { BaseSequencer, Vitest, WorkspaceSpec } from 'vitest/node';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+class Sequencer extends BaseSequencer {
+  constructor(ctx: Vitest) {
+    super(ctx);
+  }
+
+  async sort(files: WorkspaceSpec[]) {
+    files.sort((file1, file2) => file1[1].localeCompare(file2[1]));
+
+    return files;
+  }
+}
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), svgr(), tsconfigPaths()],
+  esbuild: {
+    exclude: mode === 'test' ? [] : ['src/**/*.test.*'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     include: ['./src/**/*.test.tsx'],
+    sequence: {
+      sequencer: Sequencer,
+    },
   },
-});
+}));
