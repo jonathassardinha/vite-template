b4bb41971a8fdbd20000a4bf8f1c4b5c82220d3e
Jonathas Sardinha <jsard@Jonathass-MacBook-Pro.local>
2023-06-12 23:38:17 -0300

    fix errors with vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vitest/config';
-import { BaseSequencer, Vitest, WorkspaceSpec } from 'vitest/node';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-
-class Sequencer extends BaseSequencer {
-  constructor(ctx: Vitest) {
-    super(ctx);
-  }
-
-  async sort(files: WorkspaceSpec[]) {
-    files.sort((file1, file2) => file1[1].localeCompare(file2[1]));
-
-    return files;
-  }
-}
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
@@ -28,8 +15,5 @@ export default defineConfig(({ mode }) => ({
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     include: ['./src/**/*.test.tsx'],
-    sequence: {
-      sequencer: Sequencer,
-    },
   },
 }));
